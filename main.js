ebe2a172f781b87ccb662b1fc116e98256bb3995
Ramya3106 <148340869+Ramya3106@users.noreply.github.com>
2025-07-18 13:33:00 +0530

    route structure

--- main.js
+++ main.js
@@ -7,6 +7,28 @@ app.get("/", (req, res) => {
   res.json({ msg: "Hello Students!" });
 });
 
+//CRUD functionality of movies
+
+//R - for Reading
+app.post('/movies', () =>{
+
+})
+ 
+//C - For creating movies 
+app.post('/movies', () =>{
+  
+})
+
+// U - For updating movie
+app.put('/movies/:id', () =>{
+
+})
+
+//D - For deleting movie
+app.delete('/movies/:id',() =>{
+  
+})
+
 app.listen(PORT, () => {
   console.log(`The server is running at http://localhost:${PORT}`);
 });
